44e4c57d93eff5dcfc0b13c3541d27f3371949fc
Henry Janke <henry.j@aluno.ifsc.edu.br>
2023-08-22 08:02:44 -0300

    aula pds

---
+++ teste/src/main/java/modelo/Pessoa.java
@@ -0,0 +1,23 @@
+package modelo;
+
+public class Pessoa {
+	
+	private String primeiroNome;
+	private int idFuncionario;
+	
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+	public int getIdFuncionario() {
+		return idFuncionario;
+	}
+	public void setIdFuncionario(int idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	
+	
+
+}
